854814856acfb95eb35bc2cd8c274c8da16b652c
Nicolas Cabral <ncabral@technisys.com>
2020-03-08 16:47:12 -0300

    se agregan metodos

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,83 @@
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
+import java.util.Random;
+import java.util.UUID;
 
 public class Main {
 
+	public static List<Integer> numbersBase = new ArrayList<Integer>();
+	public static List<String> stringsBase = new ArrayList<String>();
+	public static List<Object> fields = new ArrayList<Object>();
+
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
-		Person p1 = new Person(1, 21, 37993831, "Nico");
-		Car c1 = new Car(2020, 111, true, true);
-		createClass(c1);
-		generateCode("Hola");
+		Person p1 = new Person(1, 21, 37993831, "Nico", "Cabral");
+//		Car c1 = new Car(2020, 111, true, true);
+		getType(p1);
+		fields.add("123");
+		System.out.println(generateCode("10001"));
 	}
-	
-	public static String getClass(Object obj) {
-		System.out.println(obj.getClass().getName());
-		return obj.getClass().getName();
+
+	public static String convertStringsToOne(List<String> strings) {
+		String stringGlobal = "";
+		for (String string : strings) {
+			stringGlobal = stringGlobal + convertWord(string);
+		}
+		return stringGlobal;
 	}
-	
-	public static void generateCode(String string) {
-		string = string.toLowerCase();
+
+	public static String generateCode(String field) {
+		int prefix = (int) (Math.random()*100);
+		int sufix = (int) (Math.random()*100);
+		return prefix+field+sufix;
+	}
+
+	public static HashMap<Integer, Character> map() {
 		// a cada numero le asigno una letra
-		Character[] letras = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
-		int count = letras.length;
-		Integer[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27};
+		Character[] letras = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q',
+				'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+		Integer[] numeros = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47,
+				49, 51, 53 };
 		HashMap<Integer, Character> mapeo = new HashMap<Integer, Character>();
 		for (int i = 0; i < 27; i++) {
 			mapeo.put(numeros[i], letras[i]);
 		}
-		
-		System.out.println(mapeo);
+//		System.out.println(mapeo);
+		return mapeo;
+	}
+
+	public static String convertWord(String string) {
+		HashMap<Integer, Character> map = map();
+		string = string.toLowerCase();
 		// dada una palabra, me tiene que decir que numero es
-		String palabraCodificada= "";
+		String palabraCodificada = "";
 		for (int i = 0; i < string.length(); i++) {
-			for (Entry<Integer, Character> entry : mapeo.entrySet()) {
-				if(string.charAt(i)==entry.getValue()) {
-					palabraCodificada = palabraCodificada+entry.getKey();
+			for (Entry<Integer, Character> entry : map.entrySet()) {
+				if (string.charAt(i) == entry.getValue()) {
+					palabraCodificada = palabraCodificada + entry.getKey() + "0";
 				}
 			}
 		}
-		System.out.println(palabraCodificada);
-		
-//		for (Entry<Integer, Character> entry : mapeo.entrySet()) {
-//		    System.out.println("clave=" + entry.getKey() + ", valor=" + entry.getValue());
-//		}
-		
+		return palabraCodificada;
 	}
-	
-	public static void createClass(Object obj) throws IllegalArgumentException, IllegalAccessException {
-		Field[] fields = obj.getClass().getFields();
-		for(Field f : obj.getClass().getFields()) {
-			System.out.println("tipo: "+f.getType()+" field: " + f.getName()+" valor: "+f.get(obj));
-			if(f.getType() == int.class) {
-				System.out.println("es int");
+
+	public static String getNumber(List<Integer> numbers) {
+		String numberBase = "";
+		for (Integer number : numbers) {
+			numberBase = numberBase + number;
+		}
+		return numberBase;
+	}
+
+	public static void getType(Object obj) throws IllegalArgumentException, IllegalAccessException {
+		for (Field field : obj.getClass().getFields()) {
+			if (field.getType() == String.class) {
+				stringsBase.add((String) field.get(obj));
 			}
-			if(f.getType() == String.class) {
-				System.out.println("es String");
-			}
-			if(f.getType() == Integer.class) {
-				System.out.println("es Integer");
+			if (field.getType() == Integer.class) {
+				numbersBase.add((Integer) field.get(obj));
 			}
 		}
 	}
--- src/Person.java
+++ src/Person.java
@@ -1,43 +1,46 @@
+import java.util.Random;
 
 public class Person {
-	public int id;
-	public int age;
-	public int dni;
+	public Integer id;
+	public Integer age;
+	public Integer dni;
 	public String name;
+	public String lastname;
 	
-	
-	public Person() {
-	}
-	
-	public Person(int id, int age, int dni, String name) {
+	public Person(Integer id, Integer age, Integer dni, String name, String lastname) {
 		this.id = id;
 		this.age = age;
 		this.dni = dni;
 		this.name = name;
+		this.lastname = lastname;
+	}
+	
+	public String toString() {
+		   return getClass().getName() + '@' + Integer.toHexString(hashCode());
 	}
 
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public int getDni() {
-		return dni;
-	}
-
-	public void setDni(int dni) {
-		this.dni = dni;
-	}
-
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getDni() {
+		return dni;
+	}
+
+	public void setDni(Integer dni) {
+		this.dni = dni;
 	}
 
 	public String getName() {
@@ -47,5 +50,26 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 	
+	public static void main(String[] args) {
+		Random random = new Random();
+		int numero = random.nextInt(5);
+//		System.out.println(numero);
+		String field = "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+		int prefix = (int) (Math.random()*100);
+		int sufix = (int) (Math.random()*100);
+		System.out.println(prefix);
+		System.out.println(sufix);
+		
+		System.out.println(prefix+field+sufix);
+		
+	}
 }
